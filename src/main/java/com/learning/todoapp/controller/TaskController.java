1067e829580a0d0286801cfcc55857aea772eea5
Hung127 <hunghungtt05@gmail.com>
2026-09-13 16:54:42 +0700

    feat: add endpoint to list tasks

--- src/main/java/com/learning/todoapp/controller/TaskController.java
+++ src/main/java/com/learning/todoapp/controller/TaskController.java
@@ -1,7 +1,10 @@
 package com.learning.todoapp.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +39,13 @@ public class TaskController {
 
         // 201 Created
         return new ResponseEntity<TaskDto>(taskDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<TaskDto>> listTasks() {
+        List<Task> tasks = taskService.listTasks();
+        List<TaskDto> taskDtos = tasks.stream().map(taskMapper::toDto).toList();
+        return new ResponseEntity<>(taskDtos, HttpStatus.OK);
     }
 
 }
