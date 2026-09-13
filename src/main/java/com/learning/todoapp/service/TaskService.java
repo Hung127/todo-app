b9890c03b4e2fd381f5a9dadab0c2c4b02c8d041
Hung127 <hunghungtt05@gmail.com>
2026-09-13 17:40:55 +0700

    feat: add service to update and delete tasks

--- src/main/java/com/learning/todoapp/service/TaskService.java
+++ src/main/java/com/learning/todoapp/service/TaskService.java
@@ -1,12 +1,18 @@
 package com.learning.todoapp.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.learning.todoapp.domain.CreateTaskRequest;
+import com.learning.todoapp.domain.UpdateTaskRequest;
 import com.learning.todoapp.domain.entity.Task;
 
 public interface TaskService {
     public Task createTask(CreateTaskRequest request);
 
     public List<Task> listTasks();
+
+    public Task updateTask(UUID taskId, UpdateTaskRequest request);
+
+    public void deleteTask(UUID taskId);
 }
--- src/main/java/com/learning/todoapp/service/impl/TaskServiceImpl.java
+++ src/main/java/com/learning/todoapp/service/impl/TaskServiceImpl.java
@@ -1,6 +1,7 @@
 package com.learning.todoapp.service.impl;
 
 import java.util.List;
+import java.util.UUID;
 import java.time.Instant;
 
 import org.springframework.data.domain.Sort;
@@ -8,9 +9,11 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.learning.todoapp.domain.CreateTaskRequest;
+import com.learning.todoapp.domain.UpdateTaskRequest;
 import com.learning.todoapp.domain.entity.Task;
 import com.learning.todoapp.domain.entity.TaskStatus;
 import com.learning.todoapp.domain.repository.TaskRepository;
+import com.learning.todoapp.exception.TaskNotFoundException;
 import com.learning.todoapp.service.TaskService;
 
 @Service
@@ -44,5 +47,23 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public List<Task> listTasks() {
         return taskRepository.findAll(Sort.by(Direction.ASC, "created"));
+    }
+
+    @Override
+    public Task updateTask(UUID taskId, UpdateTaskRequest request) {
+        Task task = taskRepository.findById(taskId).orElseThrow(() -> new TaskNotFoundException(taskId));
+        task.setTitle(request.title());
+        task.setPriority(request.priority());
+        task.setStatus(request.status());
+        task.setDescription(request.description());
+        task.setDueDate(request.dueDate());
+        task.setUpdated(Instant.now());
+
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public void deleteTask(UUID taskId) {
+        taskRepository.deleteById(taskId);
     }
 }
